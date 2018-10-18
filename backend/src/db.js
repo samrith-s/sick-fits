5e29014fa722af8cda1377cd1208fd534501e6e8
Samrith Shankar <samrith.s@media.net>
2018-10-18 17:33:21 +0530

    Added db.js to connect yoga to prisma

---
+++ backend/src/db.js
@@ -0,0 +1,10 @@
+// Connect to remote DB and query it via JavaScript
+
+const { Prisma } = require('prisma-binding');
+
+module.exports = new Prisma({
+  typeDefs: 'src/generated/prisma.graphql',
+  endpoint: process.env.PRISMA_ENDPOINT,
+  secret: process.env.PRISMA_SECRET,
+  debug: false
+});
