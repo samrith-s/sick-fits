eeaa025930c01beb626a3369fb318366554ae181
Samrith Shankar <samrith.s@media.net>
2018-10-18 19:40:50 +0530

    Added index page

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react';
+import Link from 'next/link';
 
 export default class Home extends Component {
   render() {
-    return <div>Home!</div>;
+    return (
+      <div>
+        Home!
+        <Link href="/sell">
+          <a>Sell</a>
+        </Link>
+      </div>
+    );
   }
 }
