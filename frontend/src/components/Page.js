95b1a1f20b2845c62ec2a5a36d179b1fb1693bbb
Samrith Shankar <samrith.s@media.net>
2018-10-19 17:08:08 +0530

    Used page styles

--- frontend/src/components/Page.js
+++ frontend/src/components/Page.js
@@ -1,15 +1,27 @@
+import PropTypes from 'prop-types';
 import React, { Component } from 'react';
+import { ThemeProvider } from 'styled-components';
+
 import Header from './Header';
 import Meta from './Meta';
 
+import theme from '../styles/_theme';
+import { PageBlock, PageInner } from '../styles/page.styles';
+
 export default class Page extends Component {
+  static propTypes = {
+    children: PropTypes.node
+  };
+
   render() {
     return (
-      <div>
-        <Meta />
-        <Header />
-        {this.props.children}
-      </div>
+      <ThemeProvider theme={theme}>
+        <PageBlock>
+          <Meta />
+          <Header />
+          <PageInner>{this.props.children}</PageInner>
+        </PageBlock>
+      </ThemeProvider>
     );
   }
 }
