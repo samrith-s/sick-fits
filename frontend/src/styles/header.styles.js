33b1e97c0dee16152c2cc6ac026da5523e753e9f
Samrith Shankar <samrith.s@media.net>
2018-10-19 17:08:27 +0530

    Added header styles

---
+++ frontend/src/styles/header.styles.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const HeaderBlock = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const Logo = styled.a`
+  padding: 10px;
+  background: red;
+  color: #fff;
+  perspective: 200px;
+  transform: skew(-10deg);
+  text-transform: uppercase;
+  font-weight: bold;
+`;
