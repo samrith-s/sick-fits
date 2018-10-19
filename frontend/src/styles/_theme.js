aadb72cc5bdce49418057fc8cdad13949544fefa
Samrith Shankar <samrith.s@media.net>
2018-10-19 17:07:55 +0530

    Added theme file

---
+++ frontend/src/styles/_theme.js
@@ -0,0 +1,13 @@
+export default {
+  colors: {
+    black: '#393939',
+    gray: '#3a3a3a',
+    white: '#fefefe',
+    lightGray: '#e1e1e1',
+    offWhite: '#ededed',
+    red: '#ff0000'
+  },
+  variables: {
+    maxWidth: '1000px'
+  }
+};
