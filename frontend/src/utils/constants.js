d6ed934b8cc03b7f0e0e9deb02946ddcf963d493
Samrith Shankar <samrith.s@media.net>
2018-10-21 20:37:43 +0530

    Added items menu item

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -3,10 +3,10 @@ export const NAV_LINKS = [
     title: 'Home',
     link: '/'
   },
-  // {
-  //   title: 'Items',
-  //   link: '/items'
-  // },
+  {
+    title: 'Items',
+    link: '/items'
+  },
   {
     title: 'Sell',
     link: '/sell'
