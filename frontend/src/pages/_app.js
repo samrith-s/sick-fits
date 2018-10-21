07bd1aa773db2570aabc8cf52677365130e4443e
Samrith Shankar <samrith.s@media.net>
2018-10-21 20:36:35 +0530

    Made path query available using getInitialProps

--- frontend/src/pages/_app.js
+++ frontend/src/pages/_app.js
@@ -1,16 +1,36 @@
 import App, { Container } from 'next/app';
+import { ApolloProvider } from 'react-apollo';
+
 import Page from '../components/Page';
 
-export default class SickFits extends App {
+import withData from '../lib/withData';
+
+class SickFits extends App {
+  static async getInitialProps({ Component, context = {} }) {
+    let pageProps = {};
+
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(context);
+    }
+
+    pageProps.query = context.query;
+
+    return { pageProps };
+  }
+
   render() {
-    const { Component } = this.props;
+    const { Component, apollo, pageProps } = this.props;
 
     return (
       <Container>
-        <Page>
-          <Component />
-        </Page>
+        <ApolloProvider client={apollo}>
+          <Page>
+            <Component {...pageProps} />
+          </Page>
+        </ApolloProvider>
       </Container>
     );
   }
 }
+
+export default withData(SickFits);
