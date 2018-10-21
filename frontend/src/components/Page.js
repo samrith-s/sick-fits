5e9f254d14a024c4007d1ea0caa516fc8640452d
Samrith Shankar <samrith.s@media.net>
2018-10-21 17:55:38 +0530

    Added globalStyles component

--- frontend/src/components/Page.js
+++ frontend/src/components/Page.js
@@ -1,10 +1,11 @@
 import PropTypes from 'prop-types';
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { ThemeProvider } from 'styled-components';
 
 import Header from './Header';
 import Meta from './Meta';
 
+import GlobalStyle from '../styles/_global-style';
 import theme from '../styles/_theme';
 import { PageBlock, PageInner } from '../styles/page.styles';
 
@@ -16,11 +17,14 @@ export default class Page extends Component {
   render() {
     return (
       <ThemeProvider theme={theme}>
-        <PageBlock>
-          <Meta />
-          <Header />
-          <PageInner>{this.props.children}</PageInner>
-        </PageBlock>
+        <Fragment>
+          <PageBlock>
+            <Meta />
+            <Header />
+            <PageInner>{this.props.children}</PageInner>
+          </PageBlock>
+          <GlobalStyle />
+        </Fragment>
       </ThemeProvider>
     );
   }
