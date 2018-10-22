90f2efba9fdf840f2d96c52749eef7663f115540
Samrith Shankar <samrith.s@media.net>
2018-10-22 12:23:14 +0530

    Updated withQuery description

--- frontend/src/lib/withQuery.js
+++ frontend/src/lib/withQuery.js
@@ -1,6 +1,14 @@
 /*
   Author: Samrith Shankar
-  Description: withQuery is an HOC that helps mitigate the need for render props while using react-apollo Query
+  
+  Description: 
+  withQuery is an HOC that helps mitigate the need for render props while using react-apollo Query.
+  It's a simple wrapper which makes everything that Query provides, accessible via a payload prop on the
+  wrapped component.
+
+  `withQuery(<graphql-query>)(Component)`
+
+  `Component.props.payload` --> contains all the props provided by Query.
 */
 
 import React, { Component } from 'react';
