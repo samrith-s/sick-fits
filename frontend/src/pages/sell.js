2f03758dd20344da5f17eefda16252510645ce4c
Samrith Shankar <samrith.s@media.net>
2018-10-22 14:15:04 +0530

    Imported and used CreateItem in sell page

--- frontend/src/pages/sell.js
+++ frontend/src/pages/sell.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react';
 
+import CreateItem from '../components/CreateItem';
+
 export default class Sell extends Component {
   render() {
-    return <div>Sell!</div>;
+    return (
+      <div>
+        <h2>Sell an item</h2>
+        <CreateItem />
+      </div>
+    );
   }
 }
