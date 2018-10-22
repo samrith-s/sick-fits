a41b5b13d06298d0cae95324bfcde8765fe53bf3
Samrith Shankar <samrith.s@media.net>
2018-10-22 18:14:53 +0530

    Added cloudinary config

--- frontend/src/lib/config.js
+++ frontend/src/lib/config.js
@@ -1,2 +1,13 @@
 export const DEV_ENDPOINT = `http://localhost:4000`;
 export const PROD_ENDPOINT = `https://sickfits-yoga-prod.herokuapp.com/`;
+export const CLOUDINARY_ENDPOINT = {
+  base: 'https://api.cloudinary.com/v1_1',
+  account: 'samrithshankar',
+  url() {
+    return `${this.base}/${this.account}`;
+  },
+  upload() {
+    const url = this.url();
+    return `${url}/image/upload`;
+  }
+};
